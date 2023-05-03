c7a40de95c71e298f7534a0ed199fc3729e0694d
Mayna-B <me2be2014@gmail.com>
2023-05-02 23:08:02 -0400

    Adding places with mock data

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
+//Modules
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+//Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.render('home')
   })
-
-  app.get('*', (req, res) => {
+  
+app.get('*', (req, res) => {
     res.render('error404')
   }) 
 
-app.listen(process.env.PORT)
+//Listen for Connections
+const PORT = process.env.PORT || 3000
+
+app.listen(3000, console.log('Listening on Port 3000'))
