e92ef12beba86b1f924a3c5742cd3c3ea939db5f
Mayna-B <me2be2014@gmail.com>
2023-05-03 19:34:08 -0400

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -2,21 +2,7 @@ const router = require('express').Router()
 
 // GET /places
 router.get('/new', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
-    
-    res.render('places/index', { places })
-})
+    res.render('places/new')
+  })
 
 module.exports = router
