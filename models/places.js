dc2b5128dd70bb1f2965217ed194d5fbbd429f5c
Mayna-B <me2be2014@gmail.com>
2023-05-05 00:05:50 -0400

    added delete button

--- models/places.js
+++ models/places.js
@@ -1,10 +1,12 @@
 module.exports = [{
+    id: 1, 
     name: 'Frenchie Cafe',
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Coffee, Bakery',
     pic: '/images/frenchie-cafe.jpg'
 }, {
+    id: 2,
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
---
+++ views/places/show.jsx
@@ -0,0 +1,32 @@
+const React = require('react')
+const Def = require('../default')
+
+function show (data) {
+    let comments = (
+        <h3 className="inactive">
+          No comments yet!
+        </h3>
+      )
+      let rating = (
+        <h3 className="inactive">
+          Not yet rated
+        </h3>
+      )
+    return (
+        <Def>
+          <main>
+            <h1>{ data.place.name }</h1>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                Edit
+            </a>     
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger">
+                    Delete
+                </button>
+            </form> 
+          </main>
+        </Def>
+    )
+}
+
+module.exports = show
