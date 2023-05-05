8add2e80ba19351a16e73dbf033e0340858a543a
Mayna-B <me2be2014@gmail.com>
2023-05-05 00:09:22 -0400

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -37,5 +37,18 @@ router.get('/new', (req, res) => {
     }
   })
 
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  }) 
 
 module.exports = router
