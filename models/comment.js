4e834360bf4c6ca3c369964abdfc2cd54300c2aa
Mayna-B <me2be2014@gmail.com>
2023-05-14 16:10:30 -0400

    fixed models/index.js

--- models/comment.js
+++ models/comment.js
@@ -1,39 +1,10 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
+let commentSchema = new mongoose.Schema({
+    author: { type: String, default: 'Anonymous' },
+    rant: { type: Boolean, default: false },
+    stars: { type: Number, required: true },
+    content: { type: String, default: '' }
 })
-
-const placeSchema = new mongoose.Schema({
-    name: { 
-        type: String, 
-        required: true 
-    },
-    pic: { 
-        type: String, 
-        default: 'http://placekitten.com/350/350'
-    },
-    cuisines: { 
-        type: String, 
-        required: true 
-    },
-    city: { 
-        type: String, 
-        default: 'Anytown' 
-    },
-    state: { 
-        type: String, 
-        default: 'USA' 
-    },
-    founded: {
-      type: Number,
-      min: [1673, 'Surely not that old?!'],
-      max: [new Date().getFullYear(), 'This is the future!']
-    },
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
-  })
-
-module.exports.Place = require('./places')
-module.exports.Comment = require('./comment')
+  
+module.exports = mongoose.model('Comment', commentSchema)
